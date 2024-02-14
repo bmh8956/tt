ce90f615b460111d47aa246c31067702692e1e21
minhyeok Bang <bmh89895656@gmail.com>
2024-02-14 11:37:58 +0900

    1

---
+++ src/test/java/com/example/asdasd/AsdasdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asdasd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsdasdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
